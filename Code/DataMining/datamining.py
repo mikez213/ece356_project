76f8c0de6514ab9d4908ab090f4cb2536b83a7f8
Matthew Leung <m53leung@uwaterloo.ca>
2023-12-05 05:28:01 -0500

    train single decision tree classifier

--- Code/DataMining/datamining.py
+++ Code/DataMining/datamining.py
@@ -1,15 +1,20 @@
 import mysql.connector
 import getpass
+import pandas
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
 
 """
 input:
 vehicleType
-isLeftHandDrive
-engineCapacity
+driverPosition
 ageOfVehicle
 sexOfDriver
 ageOfDriver
-driverIMD
+propulsionType
+driverIMDDecile
+driverHomeAreaType
 
 output:
 casualty level of driver
@@ -24,29 +29,44 @@ database = input("database: ")
 username = input("username: ")
 password = getpass.getpass("password: ")
 
+# Query database for dataset
 db = mysql.connector.connect(
     host = host,
     user = username,
     password = password,
     database = database
 )
+query = """with MostSevereCasualty as (select accidentID, vehicleReference, min(casualtySeverity) as mostSevereCasualty from Casualty where casualtyClass = 1 group by accidentID, vehicleReference)
+            select vehicleType, driverPosition, ageOfVehicle, sexOfDriver, ageOfDriver, propulsionType, driverIMDDecile, driverHomeAreaType, mostSevereCasualty from Vehicle inner join MostSevereCasualty using (accidentID, vehicleReference) where 
+                vehicleType is not null and 
+                driverPosition is not null and 
+                ageOfVehicle is not null and 
+                sexOfDriver is not null and 
+                ageOfDriver is not null and 
+                propulsionType is not null and 
+                driverIMDDecile is not null and 
+                driverHomeAreaType is not null;
+            """
 
-cursor = db.cursor()
-
-query = ("show columns from Accident")
-
-cursor.execute(query)
-
-for result in cursor:
-    print(result)
-
-cursor.close()
+# load relevant data into pandas dataframe and do necessary pre-processing
+print("Fecthing data from database...")
+df = pandas.read_sql(query, db)
 db.close()
 
-# TODO: load relevant data into numpy array
+print("Pre-processing data...")
+feature_cols = ["vehicleType", "driverPosition", "ageOfVehicle", "sexOfDriver", "ageOfDriver", "propulsionType", "driverIMDDecile", "driverHomeAreaType"]
+X = df[feature_cols]
+Y = df.mostSevereCasualty
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1)
 
-# TODO: split data into k-partitions
+# Train Decision Tree classifer
+print("Training model...")
+clf = DecisionTreeClassifier()
+clf = clf.fit(X_train,y_train)
 
-# TODO: train k decision trees
+# Test decision tree classifier
+print("Testing model...")
+y_pred = clf.predict(X_test)
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 
-# TODO: validate decision trees
+# TODO: perform k fold validation
